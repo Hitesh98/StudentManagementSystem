5070bd14faee144888e1ade7ca485cf611d55889
Hitesh98 <justabhavanite@gmail.com>
2020-10-22 16:11:05 +0530

    "some commit"

--- src/com/flipkart/business/AuthService.java
+++ src/com/flipkart/business/AuthService.java
@@ -7,5 +7,7 @@ public interface AuthService {
 
     public boolean login(String username, String password) throws UserNotFoundException;
 
+    public void logout(String username);
+
     public boolean registerUser(User user, String password);
 }
--- src/com/flipkart/business/UserService.java
+++ src/com/flipkart/business/UserService.java
@@ -1,9 +1,19 @@
 package com.flipkart.business;
 
+import com.flipkart.bean.User;
+import com.flipkart.exception.UserNotFoundException;
+
 public interface UserService {
+
     default public void viewCourseCatalog() {
 
     }
+
+    public boolean register(User user, String password);
+
+    public boolean login(String username, String password) throws UserNotFoundException;
+
+    public void logout(String username);
 
 
 }
